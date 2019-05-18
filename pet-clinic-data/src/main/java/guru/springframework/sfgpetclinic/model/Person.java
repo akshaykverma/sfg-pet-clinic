71df9a2f99556c6fa6358090ed8440b0f416ba4a
akshaykverma <akshay.kverma92@gmail.com>
2019-05-18 13:19:12 +0530

    Closes #52. Add Project Lombok and Refactor

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -3,6 +3,13 @@ package guru.springframework.sfgpetclinic.model;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity{
 
@@ -11,30 +18,12 @@ public class Person extends BaseEntity{
 
 	@Column(name = "last_name")	
 	private String lastName;
-		
-	public Person() {
-		
-	}
-	
-	public String getFirstName()	 {
-		return firstName;
-	}
 
-
-	public void setFirstName(String firstName) {
+	public Person(Long id, String firstName, String lastName) {
+		super(id);
 		this.firstName = firstName;
-	}
-
-
-	public String getLastName() {
-		return lastName;
-	}
-
-
-	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 	
 	
-
 }
